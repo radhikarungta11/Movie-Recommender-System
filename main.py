9bed87b2f951eb751627da0219209808bb244d4e
Radhika Rungta <166164321+radhikarungta11@users.noreply.github.com>
2026-02-16 21:44:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st 
-import streamlit_option_menu
+import streamlit_option_menu 
 from streamlit_extras.stoggle import stoggle  
 from processing import preprocess    
 from processing.display import Main     
