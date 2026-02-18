2cea4fd4a6f7800715a76966080f196b190f40c1
Radhika Rungta <166164321+radhikarungta11@users.noreply.github.com>
2026-02-18 21:24:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 import streamlit_option_menu 
 from streamlit_extras.stoggle import stoggle    
-from processing import preprocess    
+from processing import preprocess     
 from processing.display import Main       
      
 # Setting the wide mode as default
